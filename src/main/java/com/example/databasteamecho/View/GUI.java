44e50175848c6caf959aa1a67b77a5fa63ea7d62
AbaSheger <111607144+AbaSheger@users.noreply.github.com>
2023-12-12 13:42:31 +0100

    updated project structure

--- src/main/java/com/example/databasteamecho/View/GUI.java
+++ src/main/java/com/example/databasteamecho/View/GUI.java
@@ -1,4 +1,4 @@
-package com.example.databasteamecho;
+package com.example.databasteamecho.View;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
@@ -7,7 +7,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class GUI extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
