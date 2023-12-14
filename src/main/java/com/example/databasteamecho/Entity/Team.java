17501274a07d893058c6c72bb5e251132d2b9ca2
AbaSheger <111607144+AbaSheger@users.noreply.github.com>
2023-12-14 20:45:27 +0100

    created contents of the Team class

--- src/main/java/com/example/databasteamecho/Entity/Team.java
+++ src/main/java/com/example/databasteamecho/Entity/Team.java
@@ -1,4 +1,86 @@
 package com.example.databasteamecho.Entity;
 
+import javax.persistence.*;
+
+//Abenezer
+@Entity
+
+@Table (name="teams")
 public class Team {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column (name =  "player_id")
+    private int id;
+
+
+    @Column (name= "team_name", length = 30)
+    private  String teamName;
+
+
+    @Column (name = "game_id")
+    private int gameId ;
+
+
+
+
+  public Team() {
+      
+  }
+
+  public Team(int id, String teamName, int gameId) {
+
+    this.id = id;
+    this.teamName = teamName;
+    this.gameId = gameId;
+
+    }
+
+    public int getId() {
+
+    return id;
+
+    }
+    public void setId(int id) {
+
+    this.id = id;
+
+    }
+
+    public String getTeamName() {
+
+    return teamName;
+
+    }
+
+    public void setTeamName(String teamName) {
+
+    this.teamName = teamName;
+
+    }
+
+    public int getGameId() {
+
+    return gameId;
+
+    }
+
+    public void setGameId(int gameId) {
+
+    this.gameId = gameId;
+
+    }
+
+    
+
+
+
+
+
+
+
+
+
+
+
 }
